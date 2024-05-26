fa69653a6d1d1dc4bb5827f69f88448b271befca
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-05-26 21:30:23 +0530

    feat: Add Redux toolkit for state management and refactor Counter component
    - Add "@reduxjs/toolkit" dependency to package.json
    - Import and use "counterActions" from "../store" in Counter component
    - Replace dispatch calls with counterActions in Counter component
    - Update Counter component to display counter value only when toggle is true
    - Add "incrementByFive" and "reset" actions to counterSlice in store/index.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,61 @@
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
+import { counterActions } from "../store";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
   const counter = useSelector((state) => state.counter);
+  const toggle = useSelector((state) => state.isToggle);
 
   const dispatch = useDispatch();
 
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
+
   const incrementCounterHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
+  };
+  const incrementCounterHandler2 = () => {
+    dispatch(counterActions.incrementByFive(5));
   };
 
   const decrementCounterHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
+  const resetCounterHandler = () => {
+    dispatch(counterActions.reset());
+  };
+
+    // const toggleCounterHandler = () => {
+  //   dispatch({ type: "toggle" });
+  // };
+
+  // const incrementCounterHandler = () => {
+  //   dispatch({ type: "increment" });
+  // };
+  // const incrementCounterHandler2 = () => {
+  //   dispatch({ type: "incrementByFive", payload: { value: 5 } });
+  // };
+
+  // const decrementCounterHandler = () => {
+  //   dispatch({ type: "decrement" });
+  // };
+  // const resetCounterHandler = () => {
+  //   dispatch({ type: "reset" });
+  // };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div className={classes.value}>{toggle && counter}</div>
       <div>
         <button onClick={incrementCounterHandler}>Increment</button>
+        <button onClick={incrementCounterHandler2}>Increment by 5</button>
         <button onClick={decrementCounterHandler}>Decrement</button>
       </div>
 
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={resetCounterHandler}>Reset</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,63 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state= { counter: 0 }, action) => {
-  // eslint-disable-next-line default-case
-  switch (action.type) {
-    case "increment": {
-      return { counter: state.counter + 1 };
-    }
-    case "decrement": {
-      return { counter: state.counter - 1 };
-    }
-    default: {
-      return state;
-    }
-  }
-};
+const initialState = { counter: 0, isToggle: true };
 
-const store = createStore(counterReducer);
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    incrementByFive(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.isToggle = !state.isToggle;
+    },
+    reset(state) {
+      state.counter = 0;
+    },
+  },
+});
 
-// function counterSubscriber() {
-//   const latestState = store.getState();
-//     console.log(latestState);
-// }
+// const counterReducer = (state = initialState, action) => {
+//   // eslint-disable-next-line default-case
+//   switch (action.type) {
+//     case "increment": {
+//       return { counter: state.counter + 1, isToggle: state.isToggle };
+//     }
+//     case "incrementByFive": {
+//       return {
+//         counter: state.counter + action.payload.value,
+//         isToggle: state.isToggle,
+//       };
+//     }
+//     case "decrement": {
+//       return { counter: state.counter - 1, isToggle: state.isToggle };
+//     }
+//     case "toggle": {
+//       return { isToggle: !state.isToggle, counter: state.counter };
+//     }
+//     case "reset": {
+//       return {
+//         counter: 0,
+//         isToggle: state.isToggle,
+//       };
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// };
 
-// store.subscribe(counterSubscriber);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-// store.dispatch({ type: "increment" });
-// store.dispatch({ type: "decrement" });
+export const counterActions = counterSlice.actions;
 
 export default store;
